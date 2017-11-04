3f6e00df21b7447bc43f13ec86e5a2b32a54f311
tamanugi <tamanugi@gmail.com>
2017-11-04 14:30:07 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# youtube-multi-subtitle-player
+
+[![Build Status](https://travis-ci.org/tamanugi/youtube-multi-subtitle-player.svg?branch=master)](https://travis-ci.org/tamanugi/youtube-multi-subtitle-player)
+
+> multi subtitle player for youtube
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
---
+++ src/components/Player.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class="player">
+    <div id="player-area">
+      <div id="video-player"></div>
+      <div class="caption-window caption-window-ja">
+        <span class="captions">
+          <span class="subtitle">{{ subtitle_ja }}</span>
+        </span>
+      </div>
+      <div class="caption-window">
+        <span class="captions">
+          <span class="subtitle">{{ subtitle_en }}</span>
+        </span>
+      </div>
+    </div>
+    <div class="input-group videoid-input-group">
+      <span class="input-group-addon" >https://www.youtube.com/watch?v=</span>
+      <input type="text" class="form-control" aria-describedby="basic-addon3" v-model="videoid">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button" @click="loadYoutube">
+          <span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span>
+        </button>
+      </span>
+    </div>
+    <div v-if="debug">{{ current }}</div>
+  </div>
+</template>
+
+<script>
+import YouTubePlayer from 'youtube-player'
+import http from 'http'
+import {parseString} from 'xml2js'
+
+export default {
+  name: 'player',
+  data () {
+    return {
+      videoid: '-sGiE10zNQM',
+      current: 0,
+      player: {},
+      srt: {},
+      subtitle_ja: '',
+      subtitle_en: '',
+      debug: false
+    }
+  },
+  methods: {
+    loadYoutube: function () {
+      this.player = YouTubePlayer('video-player')
+      this.player.loadVideoById(this.videoid)
+      this.player.stopVideo()
+      this.player.on('ready', () => { this.update() })
+      this.fetchLangSubtitle('ja')
+      this.fetchLangSubtitle('en')
+    },
+    fetchLangSubtitle: function (lang) {
+      let subtitleUrl = `https://www.youtube.com/api/timedtext?fmt=srv3&lang=${lang}&v=${this.videoid}`
+      console.log(subtitleUrl)
+      http.get(subtitleUrl, (res) => {
+        let body = ''
+        res.setEncoding('utf8')
+
+        res.on('data', (chunk) => {
+          body += chunk
+        })
+
+        res.on('end', (res) => {
+          parseString(body, (err, result) => {
+            if (!err) console.dir(result)
+            this.srt[lang] = result.timedtext.body[0].p
+          })
+        })
+      }).on('error', (e) => {
+        console.log(e.message) // エラー時
+      })
+    },
+    update: function () {
+      if (this.player) {
+        this.player.getCurrentTime().then(time => {
+          time = Math.floor(time * 1000)
+          this.current = time
+
+          this.subtitle_ja = this.getSubtitle('ja')
+          this.subtitle_en = this.getSubtitle('en')
+        })
+      }
+      setTimeout(this.update, 50)
+    },
+    getSubtitle: function (lang) {
+      let subtitle = ''
+      if (this.srt[lang]) {
+        for (let i = 0; i < this.srt[lang].length; i++) {
+          let sub = this.srt[lang][i]
+          let startTime = Number(sub.$.t)
+          let endTime = startTime + Number(sub.$.d)
+          if (startTime <= this.current && this.current <= endTime) {
+            subtitle = sub._
+            break
+          }
+        }
+      }
+      return subtitle
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.caption-window {
+  position: absolute;
+  text-align: center;
+  width: 640px;
+  margin: 0 auto;
+  bottom: 2%;
+  z-index: 35;
+}
+
+.caption-window-ja {
+  position: absolute;
+  text-align: center;
+  bottom: 10%;
+  z-index: 35;
+}
+
+.captions {
+  text-align: center;
+}
+
+.subtitle {
+  font-size: 16px;
+  bottom: 0px;
+  border-radius: 2px;
+  color: white;
+  fill: rgb(255, 255, 255);
+  background: rgba(8, 8, 8, 0.74902);
+  font-family: Roboto, "Arial Unicode Ms", Arial, Helvetica, Verdana, "PT Sans Caption", sans-serif;
+}
+
+#player-area {
+  position: relative;
+  z-index: 1;
+  margin: 0 auto;
+  width: 640px;
+  height: 360px;
+}
+
+.videoid-input-group {
+  width: 640px;
+  margin: 0 auto;
+  margin-top: 16px;
+}
+</style>
