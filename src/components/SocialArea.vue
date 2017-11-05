25de89eb02e34258b73c6b17a5cbe3ebae37f2bf
tamanugi <tamanugi@gmail.com>
2017-11-05 17:48:21 +0900

    ツイートボタン設置

--- src/components/SocialArea.vue
+++ src/components/SocialArea.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="root">
-    <div class="container">
-        <a href="http://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="basic-counter" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a>
-        <img class="shareyoro-img" src="../assets/share_yoro.png" >
+    <div class="socialButtons">
+      <div class="socialButtons_twitter">
+        <a :href="tweetUrl" rel="nofollow" target="_blank">
+        <img src="../assets/galmoji_twitter_icon.png" >
+        </a>
+      </div>
+      <div class="socialButtons_hatena">
+        <a href="http://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="basic-counter" title="このエントリーをはてなブックマークに追加">
+          <img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" />
+        </a>
+      </div>
     </div>
+      <img class="shareyoro-img" src="../assets/share_yoro.png" >
   </div>
 </template>
 
 <script>
 export default {
-  name: 'social-area'
+  name: 'social-area',
+  computed: {
+    tweetUrl () {
+      let hashtag = encodeURI('ぎゃるもじまくぷれ〜や〜')
+      let url = encodeURI(location.href)
+      let tweetText = encodeURI(this.$store.state.subtitle)
+      let tweetUrl = `https://twitter.com/intent/tweet?text=${tweetText}&hashtags=${hashtag}&url=${url}`
+      console.log(tweetUrl)
+      return tweetUrl
+    }
+  }
 }
 
 </script>
@@ -22,8 +41,14 @@ export default {
 
 .container {
   margin: 0 auto;
-  width: 280px;
-  height: 320px;
+}
+
+.socialButtons_twitter, .socialButtons_hatena {
+  display: inline-block;
+  vertical-align: top;
+  height: 20px;
+  overflow-y: hidden;
+  margin-bottom: 10px;
 }
 
 .shareyoro-img {
