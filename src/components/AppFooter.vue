389bfa535e082961d1646385ccb7ae9563250b49
tamanugi <tamanugi@gmail.com>
2017-11-05 17:58:34 +0900

    いらすとや クレジット

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,6 +2,8 @@
   <div>
     <hr style="margin-top: 0px;margin-bottom: 5px;">
     font by <a href="http://sozaiya405.chu.jp/405/" title="「素材屋405番地」" target="_blank">「素材屋405番地」 </a>
+    <br>
+    illust by <a href="http://www.irasutoya.com/" title="いらすとや" target="_blank">いらすとや</a>
   </div>
 </template>
 
