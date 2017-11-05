a8240b87c30159ca29d0626b1f51dfd8c8aa21cc
tamanugi <tamanugi@gmail.com>
2017-11-05 17:53:49 +0900

    フッタのhrのスタイルを調整

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <hr>
+    <hr style="margin-top: 0px;margin-bottom: 5px;">
     font by <a href="http://sozaiya405.chu.jp/405/" title="「素材屋405番地」" target="_blank">「素材屋405番地」 </a>
   </div>
 </template>
